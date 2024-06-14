939362fd76ffdb876c404a48fb01813188dd9429
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-06-14 11:17:28 +0000

    add login/logout views, isOwner permission and is_owner field to ProfileSerializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -2,8 +2,18 @@ from rest_framework import serializers
 from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
+    """
+    Reorganize data to fit JSON
+    get_is_owner checks if the user making the request is
+    the owner of the requested object
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
-        fields = ['id', 'owner', 'created_at', 'name']
+        fields = ['id', 'owner', 'created_at', 'name', 'is_owner']
--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
+from jokes.permissions import IsOwnerOrReadOnly
 
 class ProfileList(APIView):
     """
@@ -12,7 +13,10 @@ class ProfileList(APIView):
     """
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
+        serializer = ProfileSerializer(
+            profiles, 
+            many=True, 
+            context = {'request': request})
 
         return Response(serializer.data)
 
@@ -22,25 +26,37 @@ class ProfileDetail(APIView):
     get_object sees if it exists
     get returns the serialized data
     put updates the serializer with data from the request
-    serializer_class tells REST what fields are in the model (to show in the mock "frontend")
+    serializer_class tells REST what fields are in the model 
+    (to show in the mock "frontend")
+    permission_classes tells REST which users have 
+    access to what profile functions
     """
     serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
             return profile
         except:
             raise Http404
     
+
     def get(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile)
+        serializer = ProfileSerializer(
+            profile, 
+            context = {'request': request})
         return Response(serializer.data)
+
 
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(
+            profile, 
+            data=request.data, 
+            context = {'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
