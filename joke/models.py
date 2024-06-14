c4b0261edeb6668d77d47965690e2fc69145b206
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-06-14 11:45:35 +0000

    add joke model

--- joke/models.py
+++ joke/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Joke(models.Model):
+    """
+    Joke model
+    """
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=35)
+    content = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return f'{self.title} by {self.author}'
