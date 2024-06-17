ed9eee3a4760c44de59f6db410ae6e74c72252bd
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-06-17 11:44:32 +0000

    create report app and model, migrate it

--- jokes_main/settings.py
+++ jokes_main/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'joke',
     'rating',
     'followers',
+    'report',
 ]
 
 MIDDLEWARE = [
---
+++ report/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+from joke.models import Joke
+
+class Report(models.Model):
+    """
+    Report model
+    """
+    reason_choices = [
+        ('choose_an_option', 'Choose an option'),
+        ('racism','Racism'),
+        ('sexism','Sexism'),
+        ('inappropriate','Inappropriate'),
+        ('personal_attack','Personal attack')
+    ]
+
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+    joke = models.ForeignKey(
+        Joke, on_delete=models.CASCADE
+    )
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    handled = models.BooleanField(default=False)
+    reason = models.CharField(
+        max_length=32, choices=reason_choices, default='Choose an option'
+        )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner} reports {self.joke.title}"
