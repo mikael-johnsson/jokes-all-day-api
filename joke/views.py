c5bd36935cdf093d66f3a58b2253d281239c99b7
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-06-17 10:57:36 +0000

    add unused code after refactoring from joke and profiles view

--- joke/views.py
+++ joke/views.py
@@ -17,29 +17,6 @@ class JokeList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-
-
-    # def get(self, request):
-    #     jokes = Joke.objects.all()
-    #     serializer = JokeSerializer(
-    #         jokes, many=True, context={'request': request}
-    #     )
-    #     return Response(serializer.data)
-    
-
-    # def post(self, request):
-    #     serializer = JokeSerializer(
-    #         data=request.data, context={'request': request}
-    #     )
-    #     if serializer.is_valid():
-    #         serializer.save(author = request.user)
-    #         return Response(
-    #             serializer.data, status=status.HTTP_201_CREATED
-    #             )
-    #     return Response(
-    #         serializer.errors, status=status.HTTP_400_BAD_REQUEST
-    #         )
-
 class JokeDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Display one Joke
@@ -55,41 +32,3 @@ class JokeDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = JokeSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Joke.objects.all()
-
-
-    # def get_object(self, pk):
-    #     try:
-    #         joke = Joke.objects.get(pk=pk)
-    #         self.check_object_permissions(self.request, joke)
-    #         return joke
-    #     except Joke.DoesNotExist:
-    #         raise Http404
-
-
-    # def get(self, request, pk):
-    #     joke = self.get_object(pk)
-    #     serializer = JokeSerializer(
-    #         joke, context={'request': request}
-    #         )
-    #     return Response(serializer.data)
-
-    # def put(self, request, pk):
-    #     joke = self.get_object(pk)
-    #     serializer = JokeSerializer(
-    #         joke, data=request.data, context={'request': request} 
-    #     )
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(
-    #             serializer.data
-    #             )
-    #     return Response(
-    #         serializer.errors, status=status.HTTP_400_BAD_REQUEST
-    #         )
-    
-    # def delete(self, request, pk):
-    #     joke = self.get_object(pk)
-    #     joke.delete()
-    #     return Response(
-    #         status=status.HTTP_204_NO_CONTENT
-    #     )
