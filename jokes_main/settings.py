27944c81fce9ff549ad3719a9f3d9fe1265ec9be
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-06-18 08:56:37 +0000

    alter debug setting

--- jokes_main/settings.py
+++ jokes_main/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#DEBUG = 'DEV' in os.environ
+
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-mikaeljohns-jokesallday-t5n5bhicjfw.ws.codeinstitute-ide.net',
