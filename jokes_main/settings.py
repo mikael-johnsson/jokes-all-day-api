3f3dec9b97d800147e74f5c223d71498a07ae142
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-06-19 07:48:50 +0000

    add client origin dev to settings

--- jokes_main/settings.py
+++ jokes_main/settings.py
@@ -105,7 +105,8 @@ MIDDLEWARE = [
 
 if 'CLIENT_ORIGIN' in os.environ:
      CORS_ALLOWED_ORIGINS = [
-         os.environ.get('CLIENT_ORIGIN')
+         os.environ.get('CLIENT_ORIGIN'),
+         os.environ.get('CLIENT_ORIGIN_DEV')
      ] 
 
 if 'CLIENT_ORIGIN_DEV' in os.environ:
