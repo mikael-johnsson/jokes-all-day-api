5e1f522ab5dcc0730d7b31773564b447610bf4a3
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-06-27 11:56:00 +0000

    add to_representation to report serializer to get rid of underscores in reasons

--- report/serializers.py
+++ report/serializers.py
@@ -7,6 +7,11 @@ class ReportSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
     joke_title = serializers.ReadOnlyField(source='joke.title')
 
+    def to_representation(self, obj):
+        ret = super().to_representation(obj)
+        ret['reason'] = ret['reason'].replace("_", " ")
+        return ret
+
     class Meta:
         model = Report
         fields = [
