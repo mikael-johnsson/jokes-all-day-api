e8446b29c9bbdfd29770ee515cbe19ac051b4f15
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-06-27 08:12:24 +0000

    add if condition to only allow admins to see all reports

--- report/views.py
+++ report/views.py
@@ -7,7 +7,13 @@ from jokes_main.permissions import IsOwnerOrReadOnly
 class ReportList(generics.ListCreateAPIView):
     serializer_class = ReportSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Report.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Report.objects.all()
+        else:
+            return Report.objects.filter(author=user)  
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -15,4 +21,10 @@ class ReportList(generics.ListCreateAPIView):
 class ReportDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ReportSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Report.objects.all()
+    
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Report.objects.all()
+        else:
+            return Report.objects.filter(author=user) 
