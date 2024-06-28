0e078e7c3b3046a6830818607c5722c11e594ead
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-06-28 12:33:04 +0000

    change IsOwnerOrAdmin permission

--- jokes_main/permissions.py
+++ jokes_main/permissions.py
@@ -26,4 +26,4 @@ class IsOwnerOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.author == request.user or obj.author.is_staff
+        return obj.author == request.user or request.user.is_staff
